2c1a9436f356426dbd8d5303461875927d241679
Ricardo Hernandez Lopez <49227591+RIHELO@users.noreply.github.com>
2022-02-15 18:11:45 +0100

    Create yarsafactor.go
    Yet Another RSA factorization program
    
    Nothing fancy, just playing around with Golang and this is a simple program to factor primes given their multiplication
    Yes it uses BigInt and Parallel

---
+++ yarsafactor.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+ "fmt"
+ "math/big"
+ "runtime"
+ "sync"
+ "os"
+)
+
+var wg sync.WaitGroup
+
+func isEven(bgNumber *big.Int) bool {
+  bigstr := bgNumber.String()
+  last := bigstr[len(bigstr)-1:]
+  switch last {
+    case "0", "2", "4", "5", "6", "8":
+      return true
+    case "1", "3", "7", "9":
+      return false
+  }
+  return false
+}
+
+func isPerfectSquare(bgNumber *big.Int) bool {
+  bigstr := bgNumber.String()
+  last := bigstr[len(bigstr)-1:]
+  switch last {
+    case "0", "1", "4", "5", "6", "9":
+      sqrt := big.NewInt(1).Sqrt(bgNumber)
+      test := big.NewInt(1).Mod(bgNumber,sqrt)
+      if test.Cmp(big.NewInt(0)) == 0 {
+         return true
+      } else {
+         return false
+      }
+    case "2", "3", "7", "8":
+      return false
+  }
+  return false
+}
+
+func factor(process int, rsaNumber *big.Int, rng *big.Int, initialValue *big.Int) {
+  defer wg.Done()
+  fmt.Println("Process:",process, "initialValue:", initialValue, " range:", rng)
+  innerSquareSide := initialValue
+  innerSquare  := big.NewInt(0).Exp(innerSquareSide, big.NewInt(2), nil)
+  quarterSquare := big.NewInt(0).Add(innerSquare,rsaNumber)
+  for isPerfectSquare(quarterSquare) == false {
+    innerSquareSide.Add(innerSquareSide, big.NewInt(1))
+    innerSquare = big.NewInt(0).Exp(innerSquareSide, big.NewInt(2), nil)
+    quarterSquare = big.NewInt(0).Add(innerSquare,rsaNumber)
+    //fmt.Println("Process:",process, "innerSquareSide:", innerSquareSide)
+  }
+  if isPerfectSquare(quarterSquare) == true {
+    finalSquare := big.NewInt(0).Mul(big.NewInt(4),quarterSquare)
+    externalSquareSizeLength := new(big.Int).Sqrt(finalSquare)
+    innerSquareSide4 := big.NewInt(0).Sqrt(new(big.Int).Mul(innerSquare,big.NewInt(4)))
+    difference := big.NewInt(0).Sub(externalSquareSizeLength,innerSquareSide4)
+    sideP := big.NewInt(0).Div(difference,big.NewInt(2))
+    if isEven(sideP) == false {
+      sideQ := big.NewInt(0).Add(sideP,big.NewInt(1).Mul(innerSquareSide,big.NewInt(2)))
+      fmt.Println("RSA:", rsaNumber,"Process:",process, "initialValue:",initialValue, " range:", rng,"innerSquareSide: ", innerSquareSide, "p: ", sideP, "q: ", sideQ)
+      os.Exit(0)
+    }
+  }
+}
+
+func main() {
+  fmt.Println("Version: ", runtime.Version(), "NumCPU:", runtime.NumCPU(), "GOMAXPROCS",runtime.GOMAXPROCS(0))
+  var rsa = os.Args[1]
+  rsaNumber , _:= big.NewInt(0).SetString(rsa,10)
+  sqrtRsaNumber := big.NewInt(0).Sqrt(rsaNumber)
+  numProcesses := int64(8)
+  rangePerProcessor := big.NewInt(0).Div(sqrtRsaNumber,big.NewInt(numProcesses))
+  initialValue := big.NewInt(1)
+  rng := rangePerProcessor
+  for i := 0; i < int(numProcesses); i++ {
+    wg.Add(1)
+    go factor(i, rsaNumber, rng, initialValue)
+    rng = big.NewInt(0).Add(rng,rangePerProcessor)
+    initialValue = big.NewInt(0).Add(initialValue,rangePerProcessor)
+  }
+  wg.Wait()
+}
